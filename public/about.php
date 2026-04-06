d3fd651c15e3f347f5944b51844a0b6cad67cfd8
Squarenoize <odiotech@yahoo.ca>
2026-04-06 12:49:22 +0200

    about and odysee done #4

---
+++ public/about.php
@@ -0,0 +1,42 @@
+<?php
+include_once 'includes/header.php';
+?>
+    <main>
+        <section class="about">
+            <div class="about-text">
+                <p class="section-hit">NOTRE PHILOSOPHIE</p>
+                <h1>L'art de s'égarer pour mieux se <span>retrouver</span>.</h1>
+                <p class="text">Le voyage n'est pas une course contre la montre, mais une poésie du temps suspendu. Chez Vadrouille & Bourlingue, nous cultivons le luxe de la lenteur.</p>
+            </div>
+            <div class="about-image">
+                <img src="assets/img/Travel_Planner_Portrait.png" alt="Image de la travel palanner de Vadrouille & Bourlingue">
+            </div>
+        </section>
+        <section class="odysee">
+            <div class="images-cards">
+                <img src="assets/img/forest.png" alt="sentier dans la forêt">
+                <img src="assets/img/bonhomme.png" alt="personnage pour recuieillir les offrandes des pélerins">
+                <img src="assets/img/sushis.png" alt="assortiment de sushis">
+                <img src="assets/img/mario-train.png" alt="train décoré aux couleurs de Mario Bros">
+            </div>
+            <div class="odysee-text">
+                <h1>Mon Odysée</h1>
+                <p class="text">Passionnée par les cultures lointaines, j'ai
+                passé plus de deux décennies à explorer les recoins les plus confidentiels du globe.
+                Mon expertise ne se mesure pas en kilomètres parcourus, mais en émotions vécues.
+                </p>
+                <p class="text">De la sérénité des temples japonais au lever du jour à l'effervescence des marchés
+                andins, j'ai appris que le véritable luxe réside dans l'authenticité et la
+                personnalisation absolue. Vadrouille & Bourlingue est né de cette volonté de
+                proposer une alternative au tourisme de masse.
+                </p>
+                <p class="text">Chaque itinéraire que je conçois est une œuvre unique, tissée avec soin pour
+                répondre à vos aspirations les plus profondes, tout en respectant l'intégrité des
+                lieux et des hommes qui nous accueillent.
+                </p>
+            </div>
+        </section>
+    </main>
+<?php
+include_once 'includes/footer.php';
+?> 
